c8cfc92f25081b577423385f1f58a5baf291f068
swh5620 <swh5620@gmail.com>
2026-07-20 00:00:12 +0900

    soccer

--- app.py
+++ app.py
@@ -296,5 +296,8 @@ def health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=True, threaded=True)
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        debug=True
+    )
